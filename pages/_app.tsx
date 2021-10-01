af08c3bfca800792d82d14b42cf86e20df87e0eb
박인수 <humanwater@willim.me>
2021-10-01 14:01:27 +0900

    [1001] 반응형 적용 - 현재 스크린 모바일/데스크탑으로 구분

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import ScreenSwitch from '../src/helper/ScreenSwitch';
+import { useState } from 'react';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [screenMode, setScreenMode ] = useState("");
+  
+  const getScreen = (screen: string) => {
+    console.log(screen);
+    setScreenMode(screen);
+  }
+  return (
+    <>
+    <ScreenSwitch
+      getScreen = {getScreen}
+    />
+    <Component 
+            {...pageProps} 
+            screenMode = {screenMode}
+          />
+    </>
+  ) 
+        
 }
 export default MyApp
---
+++ src/helper/ScreenSwitch.tsx
@@ -0,0 +1,28 @@
+import React, {FC, useEffect} from 'react';
+import useWindowSize from '../hooks/useWindowSize';
+import styled from 'styled-components';
+const Desktop = styled.div`
+   @media (max-width: 1040px) {
+    display: none;
+  }   
+`
+const Mobile = styled.div`
+    display: none;
+    @media (max-width: 1040px){
+        display: block;
+    }
+`
+interface ScreenSwitchProps {
+    getScreen: ( screen: string ) => void;
+}
+const ScreenSwitch:React.FC<ScreenSwitchProps> = ({ getScreen }) => {
+    
+    const size = useWindowSize();
+    size ? ( size >= 1040 ? getScreen('desktop') : getScreen('mobile') ) : getScreen("") 
+    return (
+        <>
+        { size ? ( size >= 1040 ? <Desktop/> : <Mobile/> ) : "" }
+        </>
+    )
+}
+export default ScreenSwitch;
